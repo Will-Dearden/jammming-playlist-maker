c92725683f74d92e29773eb1f9ce48328b0a241e
Will Dearden <dearden98@hotmail.com>
2024-03-19 20:32:22 +0000

    pass down search results and render track list

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 import styles from "./App.module.css";
 
 function App () {
-    return (
-        <div>
-        <h1>
-          Ja<span className={styles.highlight}>mmm</span>ing
-        </h1>
-        <div className={styles.App}>
-          {/* <!-- Add a SearchBar component --> */}
-          
-          <div className={styles['App-playlist']}>
-            {/* <!-- Add a SearchResults component --> */}
-            {/* <!-- Add a Playlist component --> */}
-          </div>
+
+  const [searchResults, setSearchResults] = useState([
+    {
+      name: "example track name 1",
+      artist: "example track artist 1",
+      album: "example track album 1",
+      id: 1,
+    },
+    {
+      name: "example track name 2",
+      artist: "example track artist 2",
+      album: "example track album 2",
+      id: 2,
+    },
+    {
+      name: "example track name 3",
+      artist: "example track artist 3",
+      album: "example track album 3",
+      id: 3,
+    }
+  ])
+
+  return (
+      <div>
+      <h1>
+        Ja<span className={styles.highlight}>mmm</span>ing
+      </h1>
+      <div className={styles.App}>
+        {/* <!-- Add a SearchBar component --> */}
+        
+        <div className={styles['App-playlist']}>
+          {/* <!-- Add a SearchResults component --> */}
+          <SearchResults userSearchResults={searchResults}/>
+          {/* <!-- Add a Playlist component --> */}
         </div>
       </div>
-        );
+    </div>
+  );
 }
 
 export default App;
